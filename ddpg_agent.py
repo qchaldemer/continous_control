00e8f93db6f2b1d350a35771ad72883ef4f57dfd
root <qchalvondemersay@gmail.com>
2019-05-18 01:31:26 +0000

    add random noise & remove  tanh

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -145,7 +145,7 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([np.random.normal() for i in range(len(x))])
         self.state = x + dx
         return self.state
 
